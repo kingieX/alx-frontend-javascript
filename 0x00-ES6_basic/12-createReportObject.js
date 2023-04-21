85d2c6f360cb90ca2406ad69308cd9f87ff97556
Nnachi Kingsley <chimakingsley2162gmail.com>
2023-04-21 23:07:10 +0000

    modify changes

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -2,7 +2,7 @@ export default function createReportObject(employeesList) {
   const departmentName = Object.keys(employeesList)[0];
 
   const allEmployees = {
-    [departmentName]: [...employeesList[departmentName]],
+    [departmentName]: [...employeesList],
   };
   const getNumberOfDepartments = () => Object.keys(employeesList).length;
   return { allEmployees, getNumberOfDepartments };
