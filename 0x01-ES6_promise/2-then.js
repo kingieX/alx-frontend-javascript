735b8cd1bcca12116bc0cca9827ef3a0dfa21bf8
Nnachi Kingsley <chimakingsley2162gmail.com>
2023-04-22 00:27:33 +0000

    handles response

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,3 +1,3 @@
 export default function handleResponseFromAPI(promise) {
-  return promise.then(() => ({ status: 200, body: 'success' })).catch(() => Error('The fake API is not working currently'));
+  return promise.then(() => ({ status: 200, body: 'success' })).catch(() => ({})).finally(() => { console.log('Got a response from the API'); });
 }
