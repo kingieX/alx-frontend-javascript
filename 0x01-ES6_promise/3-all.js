8316d859bcfbaf70c3b54cd498b91d9952209062
Nnachi Kingsley <chimakingsley2162gmail.com>
2023-04-22 00:54:29 +0000

    imported uploadPhoto and createUser from utils

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()]).then((responses) => {
     const [photo, user] = responses;
-    console.log(`${user.firstName} ${user.lastName} ${photo.body}`);
+    console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
   }).catch(() => {
     console.log('Signup system offline');
   });
