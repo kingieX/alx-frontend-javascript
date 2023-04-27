fd84d968177ae25d1f018f525d2af41e99dc95a5
Nnachi Kingsley <chimakingsley2162gmail.com>
2023-04-27 16:50:42 +0000

    modify codes

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -5,3 +5,39 @@ interface Teacher {
 	readonly yearsOfExperience?: number;
 	readonly location: string;
 }
+
+interface Directors extends Teacher {
+	numberOfReports: number;
+	}
+
+interface printTeacherFunction {
+	(firstName: string, lastName: string): string;
+}
+
+function printTeacher(firstName: string, lastName: string): string {
+	const firstInitial = firstName.charAt(0).toUpperCase();
+	const formattedName = `${firstInitial}. ${lastName}`;
+	return formattedName;
+}
+
+interface StudentInterface {
+	firstName: string;
+	lastName: string;
+}
+
+interface StudentClassInterface {
+	workOnHomework(): string;
+	displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+	constructor(private student: StudentInterface) {}
+
+workOnHomework(): string {
+	return "Currently working";
+}
+
+displayName(): string {
+	return this.student.firstName;
+	}
+}
