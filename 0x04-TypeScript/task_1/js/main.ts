9b802d4d6c251fca665f68666a4d76a90cd8381f
Nnachi Kingsley <chimakingsley2162gmail.com>
2023-04-27 16:17:58 +0000

    modify changes to task 1

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,7 @@
+interface Teacher {
+	readonly firstName: string;
+	readonly lastName: string;
+	readonly fullTimeEmployee: boolean;
+	readonly yearsOfExperience?: number;
+	readonly location: string;
+}
