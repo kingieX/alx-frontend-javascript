9fa3780ebeb9cd6a12d71474e995b92faf76a70d
Nnachi Kingsley <chimakingsley2162gmail.com>
2023-05-02 16:35:22 +0000

    fix bug in 1-make_classrooms file

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,15 +1,15 @@
-import ClassRoom from './0-classroom'
+import ClassRoom from './0-classroom';
 
 function initializeRooms() {
-	const classRoomSizes = [19, 20, 34];
-	const rooms = [];
+  const classRoomSizes = [19, 20, 34];
+  const rooms = [];
 
-	for (const size of classRoomSizes) {
-		const room = new ClassRoom(size);
-		rooms.push(room)
-	}
+  for (const size of classRoomSizes) {
+    const room = new ClassRoom(size);
+    rooms.push(room);
+  }
 
-	return rooms;
+  return rooms;
 }
 
 export default initializeRooms;
