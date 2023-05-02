2a04c96cbe4c411a5dea238d9a85c519564801f7
Nnachi Kingsley <chimakingsley2162gmail.com>
2023-05-02 16:44:00 +0000

    fix eslint issue

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,35 +1,36 @@
+/* eslint-disable no-unused-vars */
 class HolbertonCourse {
-	constructor(name,length, students) {
-		if (typeof name !== 'string' || typeof length !== 'number' || !Array.isArray(students)) {
-			throw new TypeError('Invalid argument type');
-		}
+  constructor(name, length, students) {
+    if (typeof name !== 'string' || typeof length !== 'number' || !Array.isArray(students)) {
+      throw new TypeError('Invalid argument type');
+    }
 
-		this._name = name;
-		this._length = length;
-		this._students = students;
-	}
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
 
-	get name() {
-		return this._name;
-	}
+  get name() {
+    return this._name;
+  }
 
-	set name(name) {
-		this._name = name;
-	}
+  set name(name) {
+    this._name = name;
+  }
 
-	get length() {
-		return this._length;
-	}
+  get length() {
+    return this._length;
+  }
 
-	set length(length) {
-		this._length = length;
-	}
+  set length(length) {
+    this._length = length;
+  }
 
-	get students() {
-		return this._students;
-	}
+  get students() {
+    return this._students;
+  }
 
-	set students(students) {
-		this._students = students;
-	}
+  set students(students) {
+    this._students = students;
+  }
 }
