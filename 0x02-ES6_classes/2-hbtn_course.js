d0df3f1202fbb21e8db6a2783ec62b927fee79f2
Nnachi Kingsley <chimakingsley2162gmail.com>
2023-05-02 17:18:01 +0000

    fix eslint issue

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,5 +1,14 @@
 default export class HolbertonCourse {
   constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw new TypeError('length must be a number');
+    }
+    if (!Array.isArray(students) {
+      throw new TypeError('students must be an array');
+    }
     this._name = name;
     this._length = length;
     this._students = students;
